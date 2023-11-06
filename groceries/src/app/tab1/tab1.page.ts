3b45e895a6648389aa063b4b0a3f80e026eecd55
pfette1 <pfette1@live.maryville.edu>
2023-11-06 11:46:46 -0500

    added code to add and remove items

--- groceries/src/app/tab1/tab1.page.ts
+++ groceries/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,71 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  title = "Grocery";
+
+  items = [
+    {
+      name: "Milk",
+      quantity: 2
+    },
+    {
+      name: "Bread",
+      quantity: 3
+    },
+    {
+      name: "Apples",
+      quantity: 2
+    },
+    {
+      name: "Oranges",
+      quantity: 2
+    }
+  ]
+
+  public alertButtons = [
+    {
+      text: 'Cancel',
+      handler: (data: any) => {
+      }
+    },
+    {
+      text: 'Save',
+      handler: (item: any) => {
+        this.items.push(item);
+      }
+}
+  ];
+  public alertInputs = [
+    {
+      name: 'name',
+      placeholder: 'Name',
+    },
+    {
+      name: 'quantity',
+      type: 'number',
+      placeholder: 'Quantity',
+      min: 1,
+      max: 100,
+    }
+  ];
+
+  constructor(private toastController: ToastController) {}
+
+  async removeItem(item: any, index: number) {
+    console.log(item, index);
+    const toast = await this.toastController.create({
+      message: index + ' successfully removed',
+      duration: 1500,
+      position: 'bottom',
+    });
+
+    await toast.present();
+
+    this.items.splice(index, 1);
+  }
+
+  addItem() {
+    
+  }
 
 }
